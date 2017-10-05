1c979fead32421f524255692169b6d78097a0d0f
Atsushi <Atsushi@NC10>
2017-10-06 04:32:19 +0900

    文字入力

--- src/main/java/com/gocchan/testgithub/TestGithub.java
+++ src/main/java/com/gocchan/testgithub/TestGithub.java
@@ -1,10 +1,37 @@
 package com.gocchan.testgithub;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestGithub {
 
+	// NC10
+
 	public static void main(String args[]) {
-		// NC10
-		System.out.println("Yes!");
+
+
+		getInput();
+	}
+
+	private static void getInput() {
+
+		String str = null;
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	    try{
+	    	str = br.readLine();
+	    	if(str.equals("exit")) {
+	    		System.out.println("### Ended ###");
+	    	} else {
+	    		System.out.println("input: '" + str + "'");
+	    		getInput();
+	    	}
+
+	    }catch(IOException e){
+	    	System.out.println("### Error ###:" + e.getMessage());
+	    }
+
 	}
 }
 

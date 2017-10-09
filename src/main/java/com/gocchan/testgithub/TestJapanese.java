8f2fbad0b99d619dae2a87b9874fef6b29e454d9
A_GOTO <a_goto@gocchan.com>
2017-10-09 09:22:50 +0900

    ん 対応

--- src/main/java/com/gocchan/testgithub/TestJapanese.java
+++ src/main/java/com/gocchan/testgithub/TestJapanese.java
@@ -36,63 +36,67 @@ public class TestJapanese {
     		put("x", Arrays.asList("ぁ", "ぃ", "ぅ", "ぇ", "ぉ"));
     		put("v", Arrays.asList("ゔぁ", "ゔぃ", "ゔ", "ゔぇ", "ゔぉ"));
     		put("q", Arrays.asList("くぁ", "くぃ", "く", "くぇ", "くぉ"));
-    		put("t’", Arrays.asList("", "てぃ", "とぅ", "", ""));
-    		put("d’", Arrays.asList("", "でぃ", "どぅ", "", ""));
     		put("l", Arrays.asList("ぁ", "ぃ", "ぅ" , "ぇ" , "ぉ"));
     		put("c", Arrays.asList("か", "し", "く", "せ", "こ"));
     	}
     };
 
     private static final Map<String, List<String>> ROMAN_2 = new HashMap<String, List<String>>() {
-    	{put("wy", Arrays.asList("", "ゐ", "", "ゑ", ""));}
-    	{put("ky", Arrays.asList("きゃ", "きぃ", "きゅ", "きぇ", "きょ"));}
-    	{put("gy", Arrays.asList("ぎゃ", "ぎぃ", "ぎゅ", "ぎぇ", "ぎょ"));}
-    	{put("sy", Arrays.asList("しゃ", "しぃ", "しゅ", "しぇ", "しょ"));}
-    	{put("sh", Arrays.asList("しゃ", "し", "しゅ", "しぇ", "しょ"));}
-    	{put("zy", Arrays.asList("じゃ", "じぃ", "じゅ", "じぇ", "じょ"));}
-    	{put("ty", Arrays.asList("ちゃ", "ちぃ", "ちゅ", "ちぇ", "ちょ"));}
-    	{put("ch", Arrays.asList("ちゃ", "ち", "ちゅ", "ちぇ", "ちょ"));}
-    	{put("dy", Arrays.asList("ぢゃ", "ぢぃ", "ぢゅ", "ぢぇ", "ぢょ"));}
-    	{put("ny", Arrays.asList("にゃ", "にぃ", "にゅ", "にぇ", "にょ"));}
-    	{put("hy", Arrays.asList("ひゃ", "ひぃ", "ひゅ", "ひぇ", "ひょ"));}
-    	{put("by", Arrays.asList("びゃ", "びぃ", "びゅ", "びぇ", "びょ"));}
-    	{put("py", Arrays.asList("ぴゃ", "ぴぃ", "ぴゅ", "ぴぇ", "ぴょ"));}
-    	{put("my", Arrays.asList("みゃ", "みぃ", "みゅ", "みぇ", "みょ"));}
-    	{put("ry", Arrays.asList("りゃ", "りぃ", "りゅ", "りぇ", "りょ"));}
-    	{put("ts", Arrays.asList("つぁ", "つぃ", "つ", "つぇ", "つぉ"));}
-    	{put("th", Arrays.asList("てゃ", "てぃ", "てゅ", "てぇ", "てょ"));}
-    	{put("dh", Arrays.asList("でゃ", "でぃ", "でゅ", "でぇ", "でょ"));}
-    	{put("xk", Arrays.asList("ヵ", "", "", "ヶ", ""));}
-    	{put("xt", Arrays.asList("", "", "っ", "", ""));}
-    	{put("xy", Arrays.asList("ゃ", "ぃ", "ゅ", "ぇ", "ょ"));}
-    	{put("xw", Arrays.asList("ゎ", "", "", "", ""));}
-    	{put("wh", Arrays.asList("うぁ", "うぃ", "う", "うぇ", "うぉ"));}
-    	{put("vy", Arrays.asList("ゔゃ", "ゔぃ", "ゔゅ", "ゔぇ", "ゔょ"));}
-    	{put("kw", Arrays.asList("くぁ", "くぃ", "くぅ", "くぇ", "くぉ"));}
-    	{put("gw", Arrays.asList("ぐぁ", "ぐぃ", "ぐぅ", "ぐぇ", "ぐぉ"));}
-    	{put("jy", Arrays.asList("じゃ", "じぃ", "じゅ", "じぇ", "じょ"));}
-    	{put("cy", Arrays.asList("ちゃ", "ちぃ", "ちゅ", "ちぇ", "ちょ"));}
-    	{put("tw", Arrays.asList("とぁ", "とぃ", "とぅ", "とぇ", "とぉ"));}
-    	{put("dw", Arrays.asList("どぁ", "どぃ", "どぅ", "どぇ", "どぉ"));}
-    	{put("hw", Arrays.asList("ふぁ", "ふぃ", "", "ふぇ", "ふぉ"));}
-    	{put("fy", Arrays.asList("ふゃ", "ふぃ", "ふゅ", "ふぇ", "ふょ"));}
-    	{put("lk", Arrays.asList("ヵ", "", "", "ヶ", ""));}
-    	{put("lt", Arrays.asList("", "", "っ", "", ""));}
-    	{put("ly", Arrays.asList("ゃ", "ぃ", "ゅ", "ぇ", "ょ"));}
-    	{put("lw", Arrays.asList("ゎ", "", "", "", ""));}
-    	{put("qw", Arrays.asList("くぁ", "くぃ", "くぅ", "くぇ", "くぉ"));}
-    	{put("sw", Arrays.asList("すぁ", "すぃ", "すぅ", "すぇ", "すぉ"));}
-    	{put("zw", Arrays.asList("ずぁ", "ずぃ", "ずぅ", "ずぇ", "ずぉ"));}
-    	{put("fw", Arrays.asList("ふぁ", "ふぃ", "ふぅ", "ふぇ", "ふぉ"));}
-    	{put("qy", Arrays.asList("くゃ", "くぃ", "くゅ", "くぇ", "くょ"));}
+    	{
+	    	put("wy", Arrays.asList("", "ゐ", "", "ゑ", ""));
+	    	put("ky", Arrays.asList("きゃ", "きぃ", "きゅ", "きぇ", "きょ"));
+	    	put("gy", Arrays.asList("ぎゃ", "ぎぃ", "ぎゅ", "ぎぇ", "ぎょ"));
+	    	put("sy", Arrays.asList("しゃ", "しぃ", "しゅ", "しぇ", "しょ"));
+	    	put("sh", Arrays.asList("しゃ", "し", "しゅ", "しぇ", "しょ"));
+	    	put("zy", Arrays.asList("じゃ", "じぃ", "じゅ", "じぇ", "じょ"));
+	    	put("ty", Arrays.asList("ちゃ", "ちぃ", "ちゅ", "ちぇ", "ちょ"));
+	    	put("ch", Arrays.asList("ちゃ", "ち", "ちゅ", "ちぇ", "ちょ"));
+	    	put("dy", Arrays.asList("ぢゃ", "ぢぃ", "ぢゅ", "ぢぇ", "ぢょ"));
+	    	put("ny", Arrays.asList("にゃ", "にぃ", "にゅ", "にぇ", "にょ"));
+	    	put("hy", Arrays.asList("ひゃ", "ひぃ", "ひゅ", "ひぇ", "ひょ"));
+	    	put("by", Arrays.asList("びゃ", "びぃ", "びゅ", "びぇ", "びょ"));
+	    	put("py", Arrays.asList("ぴゃ", "ぴぃ", "ぴゅ", "ぴぇ", "ぴょ"));
+	    	put("my", Arrays.asList("みゃ", "みぃ", "みゅ", "みぇ", "みょ"));
+	    	put("ry", Arrays.asList("りゃ", "りぃ", "りゅ", "りぇ", "りょ"));
+	    	put("ts", Arrays.asList("つぁ", "つぃ", "つ", "つぇ", "つぉ"));
+	    	put("th", Arrays.asList("てゃ", "てぃ", "てゅ", "てぇ", "てょ"));
+	    	put("dh", Arrays.asList("でゃ", "でぃ", "でゅ", "でぇ", "でょ"));
+	    	put("xk", Arrays.asList("ヵ", "", "", "ヶ", ""));
+	    	put("xt", Arrays.asList("", "", "っ", "", ""));
+	    	put("xy", Arrays.asList("ゃ", "ぃ", "ゅ", "ぇ", "ょ"));
+	    	put("xw", Arrays.asList("ゎ", "", "", "", ""));
+	    	put("wh", Arrays.asList("うぁ", "うぃ", "う", "うぇ", "うぉ"));
+	    	put("vy", Arrays.asList("ゔゃ", "ゔぃ", "ゔゅ", "ゔぇ", "ゔょ"));
+	    	put("kw", Arrays.asList("くぁ", "くぃ", "くぅ", "くぇ", "くぉ"));
+	    	put("gw", Arrays.asList("ぐぁ", "ぐぃ", "ぐぅ", "ぐぇ", "ぐぉ"));
+	    	put("jy", Arrays.asList("じゃ", "じぃ", "じゅ", "じぇ", "じょ"));
+	    	put("cy", Arrays.asList("ちゃ", "ちぃ", "ちゅ", "ちぇ", "ちょ"));
+	    	put("tw", Arrays.asList("とぁ", "とぃ", "とぅ", "とぇ", "とぉ"));
+	    	put("dw", Arrays.asList("どぁ", "どぃ", "どぅ", "どぇ", "どぉ"));
+	    	put("hw", Arrays.asList("ふぁ", "ふぃ", "", "ふぇ", "ふぉ"));
+	    	put("fy", Arrays.asList("ふゃ", "ふぃ", "ふゅ", "ふぇ", "ふょ"));
+	    	put("lk", Arrays.asList("ヵ", "", "", "ヶ", ""));
+	    	put("lt", Arrays.asList("", "", "っ", "", ""));
+	    	put("ly", Arrays.asList("ゃ", "ぃ", "ゅ", "ぇ", "ょ"));
+	    	put("lw", Arrays.asList("ゎ", "", "", "", ""));
+	    	put("qw", Arrays.asList("くぁ", "くぃ", "くぅ", "くぇ", "くぉ"));
+	    	put("sw", Arrays.asList("すぁ", "すぃ", "すぅ", "すぇ", "すぉ"));
+	    	put("zw", Arrays.asList("ずぁ", "ずぃ", "ずぅ", "ずぇ", "ずぉ"));
+	    	put("fw", Arrays.asList("ふぁ", "ふぃ", "ふぅ", "ふぇ", "ふぉ"));
+	    	put("qy", Arrays.asList("くゃ", "くぃ", "くゅ", "くぇ", "くょ"));
+			put("t'", Arrays.asList("", "てぃ", "とぅ", "", ""));
+			put("d'", Arrays.asList("", "でぃ", "どぅ", "", ""));
+    	}
     };
 
     private static final Map<String, List<String>> ROMAN_3 = new HashMap<String, List<String>>() {
-    	{put("hwy", Arrays.asList("", "", "ふゅ", "", ""));}
-    	{put("xts", Arrays.asList("", "", "っ", "", ""));}
-    	{put("lts", Arrays.asList("", "", "っ", "", ""));}
-    	{put("t’y", Arrays.asList("", "", "てゅ", "", ""));}
-    	{put("d’y", Arrays.asList("", "", "でゅ", "", ""));}
+	    {
+	    	put("hwy", Arrays.asList("", "", "ふゅ", "", ""));
+	    	put("xts", Arrays.asList("", "", "っ", "", ""));
+	    	put("lts", Arrays.asList("", "", "っ", "", ""));
+	    	put("t'y", Arrays.asList("", "", "てゅ", "", ""));
+	    	put("d'y", Arrays.asList("", "", "でゅ", "", ""));
+	    }
     };
 
     private static Map<String, List<String>> ROMAN(int i) {
@@ -125,27 +129,35 @@ public class TestJapanese {
 			if(BOIN.contains(now)) {
 
 				if(ROMAN(cnt).containsKey(buf)) {
-
-
-					//out = ROMAN(1).get("m").get(BOIN.indexOf("e"));
-
 					out += ROMAN(cnt).get(buf).get(BOIN.indexOf(now));
-
 					cnt = 0;
 					buf = "";
 				}
 
 			} else {
-				buf += now;
-				cnt++;
 
+				if(cnt > 3) {
+					out += buf.substring(0, 1); // 1文字目から1文字
+					buf = buf.substring(1) + now; // 2文字目からとカレント文字
+				} else {
 
-
-
+					if(buf.equals("n")) {
+						out += "ん";
+						if(now.equals("n") || now.equals("'")) {
+							buf = "";
+							cnt = 0;
+						} else {
+							buf = now;
+						}
+					} else {
+						buf += now;
+						cnt++;
+					}
+				}
 			}
 		}
 
-		return out;
+		return out + buf;
 
     }
 }

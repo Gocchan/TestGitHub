b476185272ca713d7cf2f921b19ada87600f141d
A_GOTO <master@gocchan.com>
2018-06-14 07:07:41 +0900

    初期化
    メインクラス以外を全て削除

--- src/main/java/com/gocchan/testgithub/TestGithub.java
+++ src/main/java/com/gocchan/testgithub/TestGithub.java
@@ -2,33 +2,7 @@ package com.gocchan.testgithub;
 
 public class TestGithub {
 
-	private static TestGithub hGithub;
-	private static TestGoogleAPI hGoogle;
-	private static TestJapanese hJapanese;
-	private static TestGinput hGinput;
-
 	public static void main(String args[]) {
 
-		// 順番大事！
-		hGithub = new TestGithub();
-		hGoogle = new TestGoogleAPI();
-		hJapanese = new TestJapanese(hGithub);
-		hGinput = new TestGinput(hGithub);
-
-
-		//System.setProperty("file.encoding", "UTF-8");
-		hGinput.getInput();
-	}
-
-	public TestGinput getGinputHandler() {
-	    return hGinput;
-	}
-	public TestGoogleAPI getGoogleHandler() {
-	    return hGoogle;
-	}
-	public TestJapanese getJapaneseHandler() {
-	    return hJapanese;
 	}
 }
-
-
